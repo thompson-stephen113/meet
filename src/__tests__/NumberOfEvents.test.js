41a450379d8eb17d3e557434ef456ca51e0537f3
Stephen Thompson <thompson.stephen113@gmail.com>
2023-12-15 11:26:04 -0500

    created NumberOfEvents.test.js created NumberOfEvents.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/no-render-in-setup */
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NumberOfEvents from "../components/NumberOfEvents";
+
+describe("<NumberOfEvents /> component", () => {
+    let NumberOfEventsComponent;
+
+    beforeEach(() => {
+        NumberOfEventsComponent = render(<NumberOfEvents />);
+    });
+
+    // ------------------------- TESTS ------------------------- //
+    // Test "textbox element is present in NumberOfEvents"
+    test("textbox element is present in NumberOfEvents", () => {
+        const input = NumberOfEventsComponent.queryByRole("textbox");
+        expect(input).toBeInTheDocument();
+    });
+
+    // Test "by default, 32 events will be displayed"
+    test("by default, 32 events will be displayed", () => {
+        const input = NumberOfEventsComponent.queryByRole("textbox");    
+        expect(input).toHaveValue("32");
+    });
+
+    // Test "user changes the number of events displayed"
+    test("user changes the number of events displayed", async () => {
+        const input = NumberOfEventsComponent.queryByRole("textbox");
+        await userEvent.type(input, "{backspace}{backspace}10");
+        expect(input).toHaveValue("10");
+    });
+});
---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,9 @@
+const NumberOfEvents = () => {
+    return (
+        <div id="number-of-events">
+            <input type="text" defaultValue="32"></input>
+        </div>
+    );
+};
+
+export default NumberOfEvents;
