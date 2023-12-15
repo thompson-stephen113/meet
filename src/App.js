945da6d3d354af4f3934975aa83f55c56cbc01d5
Stephen Thompson <thompson.stephen113@gmail.com>
2023-12-15 10:29:54 -0500

    updated:   package-lock.json     updated:   package.json 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	updated:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/logo.svg 	updated:   src/setupTests.js 	src/__tests__/ 	src/api.js 	src/components/ 	src/mock-data.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EventList />
+      <CitySearch />
     </div>
   );
 }
