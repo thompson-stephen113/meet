61c630576db6c1036398d3a3d229ba9705735a77
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-04 10:59:48 -0500

    added integration test to render number of events equal to user input

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -63,4 +63,19 @@ describe("<App /> integration", () => {
             expect(event.textContent).toContain("Berlin, Germany");
         });
     });
+
+    // Test "renders a number of events equal to the number of events inputted by the user"
+    test("renders a number of events equal to the number of events inputted by the user", async () => {
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+
+        const NumberOfEventsDOM = AppDOM.querySelector("#number-of-events");
+        const NumberOfEventsInput = within(NumberOfEventsDOM).queryByRole("textbox");
+
+        await userEvent.type(NumberOfEventsInput, "{backspace}{backspace}10");
+
+        const EventListDOM = AppDOM.querySelector("#event-list");
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole("listitem");
+        expect(allRenderedEventItems.length).toBe(10);
+    });
 });
