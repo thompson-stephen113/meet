5fff473de84284859acce01e1301b704d3a50af7
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-04 11:01:15 -0500

    received setCurrentNOE and created handleInputChanged to set its value

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,16 @@
-const NumberOfEvents = () => {
+const NumberOfEvents = ({ setCurrentNOE }) => {
+    const handleInputChanged = (event) => {
+        const value = event.target.value;
+        setCurrentNOE(value);
+    };
+
     return (
         <div id="number-of-events">
-            <input type="text" defaultValue="32"></input>
+            <input
+                type="text" 
+                defaultValue="32" 
+                onChange={handleInputChanged}
+            />
         </div>
     );
 };
