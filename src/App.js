88f44be65af9a08b4338c41cd562995d658d7c81
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-04 10:58:48 -0500

    added currentNOE to useEffect dependency array
    passed setCurrentNOE to NumberOfEvents component

--- src/App.js
+++ src/App.js
@@ -7,47 +7,45 @@ import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 const App = () => {
-  // Sets event state
-  const [events, setEvents] = useState([]);
+	// Sets event state
+	const [events, setEvents] = useState([]);
 
-  // Sets current number of events state
-  const [currentNOE, setCurrentNOE] = useState(32);
+	// Sets current number of events state
+	const [currentNOE, setCurrentNOE] = useState(32);
 
-  // Sets allLocations state
-  const [allLocations, setAllLocations] = useState([]);
+	// Sets allLocations state
+	const [allLocations, setAllLocations] = useState([]);
 
-  // Sets state for the current city
-  const [currentCity, setCurrentCity] = useState("See all cities");
+	// Sets state for the current city
+	const [currentCity, setCurrentCity] = useState("See all cities");
 
-  // Populates the events state with the fetched events list
-  const fetchData = async () => {
-    const allEvents = await getEvents();
+	// Populates the events state with the fetched events list
+	const fetchData = async () => {
+		const allEvents = await getEvents();
 
-    // Filters out list of events based on value of currentCity, otherwise renders all events
-    if (allEvents) {
-      const filteredEvents = currentCity === "See all cities" ?
-        allEvents :
-        allEvents.filter(event => event.location === currentCity)
+		// Filters out list of events based on value of currentCity, otherwise renders all events
+		const filteredEvents = currentCity === "See all cities" ?
+		allEvents :
+		allEvents.filter(event => event.location === currentCity);
 
-      setEvents(filteredEvents.slice(0, currentNOE));
+		setEvents(filteredEvents.slice(0, currentNOE));
 
-      // Initializes allLocations state
-      setAllLocations(extractLocations(allEvents));
-    };
-  };
+		// Initializes allLocations state
+		setAllLocations(extractLocations(allEvents));
+	};
 
-  // Calls fetchData() when the App component is mounted
-  useEffect(() => {
-    fetchData();
-  }, [currentCity]);
+  	// Calls fetchData() when the App component is mounted
+	useEffect(() => {
+		fetchData();
+	}, [currentCity, currentNOE]);
 
-  return (
-    <div className="App">
-      <EventList events={events}/>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
-      <NumberOfEvents />
-    </div>
-  );
-}
+	return (
+		<div className="App">
+		<EventList events={events}/>
+		<CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
+		<NumberOfEvents setCurrentNOE={setCurrentNOE}/>
+		</div>
+	);
+};
 
 export default App;
