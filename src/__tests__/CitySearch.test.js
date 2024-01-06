b7ba2e0574757afcc6c816a2bf655effdc201872
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-06 15:58:06 -0500

    removed comments for disabling eslint rules

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,7 +1,3 @@
-/* eslint-disable testing-library/no-node-access */
-/* eslint-disable testing-library/no-render-in-setup */
-/* eslint-disable testing-library/prefer-screen-queries */
-/* eslint-disable testing-library/render-result-naming-convention */
 import { render, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { extractLocations, getEvents } from "../api";
