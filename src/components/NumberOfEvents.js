f657915aeecac8f50f20ea77b39a7a55fe53e84d
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-16 14:13:04 -0500

    extracted setErrorAlert and created a conditional for errorText

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,17 @@
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
     const handleInputChanged = (event) => {
         const value = event.target.value;
         setCurrentNOE(value);
+
+        // Returns an error if number of events is invalid
+        let errorText;
+        if (isNaN(value) || value <= 0) {
+            errorText = "Only positive numbers are allowed."
+        } else {
+            errorText = ""
+        }
+
+        setErrorAlert(errorText);
     };
 
     return (
