a21a5e27f4ddfbf2c77c4b9ea985ae24c3696da7
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-18 13:22:12 -0500

    created WarningAlert subclass

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -50,4 +50,13 @@ class ErrorAlert extends Alert {
     }
 }
 
-export { InfoAlert, ErrorAlert };
+// WarningAlert subclass
+class WarningAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = "rgb(255, 255, 0)"; // yellow
+        this.bgColor = "rgb(255, 165, 0)" // orange
+    }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
