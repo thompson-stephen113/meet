0c056806d2da1112db77ac1b273e2fff990dbb0e
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-18 13:29:13 -0500

    inverted colors of WarningAlert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -54,8 +54,8 @@ class ErrorAlert extends Alert {
 class WarningAlert extends Alert {
     constructor(props) {
         super(props);
-        this.color = "rgb(255, 255, 0)"; // yellow
-        this.bgColor = "rgb(255, 165, 0)" // orange
+        this.color = "rgb(255, 165, 0)"; // orange
+        this.bgColor = "rgb(255, 255, 0)" // yellow
     }
 }
 
