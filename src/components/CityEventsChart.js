1640ffd294e06ccd38863f8c40374e6591da4c28
Stephen Thompson <thompson.stephen113@gmail.com>
2024-01-23 08:52:45 -0500

    added adjustments to XAxis ticks

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -37,7 +37,14 @@ const CityEventsChart = ({ allLocations, events}) => {
                 }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City"/>
+                <XAxis
+                    type="category"
+                    dataKey="city"
+                    name="City"
+                    angle={60}
+                    interval={0}
+                    tick={{ dx: 20, dy: 40, fontSize: 14 }}    
+                />
                 <YAxis
                     type="number"
                     dataKey="count"
